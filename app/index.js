ba935ec0ed40a7307076128c0aec694c3ea02106
Robert-96 <dezmereanrobert@gmail.com>
2023-03-01 02:16:09 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,46 @@
+(async function() {
+  const ws = await connectToServer();
+
+  document.body.onmousemove = (evt) => {
+    const messageBody = { x: evt.clientX, y: evt.clientY };
+    ws.send(JSON.stringify(messageBody));
+  };
+
+  ws.onmessage = (webSocketMessage) => {
+    const messageBody = JSON.parse(webSocketMessage.data);
+    const cursor = getOrCreateCursorFor(messageBody);
+    cursor.style.transform = `translate(${messageBody.x}px, ${messageBody.y}px)`;
+  };
+
+  async function connectToServer() {
+    const ws = new WebSocket("ws://localhost:7071/ws");
+
+    return new Promise((resolve, reject) => {
+      const timer = setInterval(() => {
+        if (ws.readyState === 1) {
+          clearInterval(timer)
+          resolve(ws);
+        }
+      }, 10);
+    });
+  }
+
+  function getOrCreateCursorFor(messageBody) {
+    const sender = messageBody.sender;
+    const existing = document.querySelector(`[data-sender="${sender}"]`);
+
+    if (existing) {
+      return existing;
+    }
+
+    const template = document.getElementById("cursor");
+    const cursor = template.content.firstElementChild.cloneNode(true);
+    const svgPath = cursor.getElementsByTagName("path")[0];
+
+    cursor.setAttribute("data-sender", sender);
+    svgPath.setAttribute("fill", `hsl(${messageBody.color}, 50%, 50%)`);
+    document.body.appendChild(cursor);
+
+    return cursor;
+  }
+})();
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 7071 });
+const clients = new Map();
+
+wss.on('connection', (ws) => {
+  const id = uuidv4();
+  const color = Math.floor(Math.random() * 360);
+  const metadata = { id, color };
+
+  clients.set(ws, metadata);
+
+  ws.on('message', (messageAsString) => {
+    const message = JSON.parse(messageAsString);
+    const metadata = clients.get(ws);
+
+    message.sender = metadata.id;
+    message.color = metadata.color;
+
+    const outbound = JSON.stringify(message);
+
+    [...clients.keys()].forEach((client) => {
+      client.send(outbound);
+    });
+  });
+
+  ws.on("close", () => {
+    clients.delete(ws);
+  });
+});
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
